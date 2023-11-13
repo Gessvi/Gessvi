b5b748a9ded144941dfc54e61d95214b61fdd6cf
Gessvi <143418965+Gessvi@users.noreply.github.com>
2023-11-13 10:28:57 -0300

    Update Vetores nome.c

--- Vetores nome.c
+++ Vetores nome.c
@@ -8,31 +8,30 @@ int main (){
 
 //variavel
 int idade [5];
-char s [10];
+char s [200] [5];
 int i;
 
 for ( i = 0; i < 5; i++)
-{
-    printf ("Idade %iº:", i + 1);
+{ 
+	printf ("Insira seu nome: \n ");
+    gets(s[i]);
+    
+    printf ("Idade %iÂº:", i+1);
     scanf ("%i", &idade[i]);
+	
+	fflush (stdin);
 }
 
 printf (" \n Exibindo dados \n");
 
 for (i = 0; i < 5; i++)
-
-printf ("Idade [%i] %i \n", i + 1, idade[i]);
-
-
-for (i = 0; i < 5; i++)
 {
-    printf ("Insira seu nome: \n ");
-    scanf ("%s", s);
+   printf ("Idade [%i]: %i \n", i + 1, idade[i]);
+   
+   printf ("Nome: %s \n \n",s[i]);
 }
 
-fflush (stdin);
 
-printf ("Nome: %s \n \n",s);
 
 
 return 0;

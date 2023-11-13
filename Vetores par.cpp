14255f31027ba5249ec36939ba0c877a2732ece5
Gessvi <143418965+Gessvi@users.noreply.github.com>
2023-11-13 10:57:45 -0300

    Add files via upload

---
+++ Vetores par.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int main () {
+	setlocale (LC_ALL, "portuguese");
+	
+	int i;
+	int numero[6];
+	int par;
+	int impar;
+	
+for (i = 0; i < 5; i++)
+	{
+		printf ("Insira o numero");
+		scanf ("%d", &numero[i]);
+		
+		if (numero[i] % 2 == 0)
+		{
+			par ++;
+		}
+		else 
+		{
+		impar ++;
+		}
+	}
+
+	printf ("\n Exibindo os dados informados... \n");
+	
+	for (i = 0; i < 6; i++)
+	{
+		printf ("Resultado: %d \n", i + 1, numero[i]);
+	}
+	
+	printf ("Numeros par: %d \n", par);
+	printf ("numeros impar: %d \n", impar);
+	
+	
+return 0;
+}
---
+++ vetores maior.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+	setlocale (LC_ALL, "portuguese");
+	
+	int i;
+	int numero[5];
+	int maior;
+	int menor;
+	
+	
+	for (i = 0; i < 5; i++)
+	{
+		printf ("Insira o numero:"), i+1;
+		scanf ("%d", numero[5]);
+		
+		maior = maior > numero[i] ? maior : numero[i];
+		menor = menor < numero[i] ?  menor : numero[i];
+		
+	}
+	
+	printf ("\n Exibindo dados \n");
+	 
+	for (i = 0; i < 5; i++)
+	{
+		print ("Resultado: %d \n", i + 1, numero[i]);
+	}
+	
+	printf ("maior: %d \n", maior);
+	printf ("menor: %d \n", menor);
+	
+
+	
+return 0;
+}

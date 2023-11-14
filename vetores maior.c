59bef8cf0cb61f9bfa9f0161eb73e4240539eb08
Gessvi <143418965+Gessvi@users.noreply.github.com>
2023-11-14 18:05:58 -0300

    Update vetores maior.c

--- vetores maior.c
+++ vetores maior.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include <locale.h>
 
-int main (){
-	setlocale (LC_ALL, "portuguese");
-	
-	int i;
-	int numero[5];
-	int maior;
-	int menor;
-	
-	
-	for (i = 0; i < 5; i++)
-	{
-		printf ("Insira o numero:", i + 1);
-		scanf ("%d", numero[i]);
-		
-		maior = maior > numero[i] ? maior : numero[i];
-		menor = menor < numero[i] ?  menor : numero[i];
-		
-	}
-	
-	printf ("\n Exibindo dados \n");
-	 
-	for (i = 0; i < 5; i++)
-	{
-		print ("Resultado: %d \n", i + 1, numero[i]);
-	}
-	
-	printf ("maior: %d \n", maior);
-	printf ("menor: %d \n", menor);
-	
+int main () {
+int i;
+int numero [5];
+int maior = 0;
+int menor = 9999;
 
-	
+for ( i = 0; i < 5; i++)
+{
+    printf ("Insria um numero:", i + 1);
+    scanf ("%d", &numero[i]);
+
+    maior = numero[i] > maior ? numero[i] : maior;
+    menor = menor < numero[i] ? menor : numero[i];
+}
+
+printf (" \n Exibindo dados \n");
+
+for (i = 0; i < 5; i++)
+{
+    printf ("Elementos [%d] %d \n", i + 1, numero[i]);
+}
+
+printf ("Maior Numero: %d \n", maior);
+printf ("Menor Numero: %d \n", menor);
 return 0;
 }
